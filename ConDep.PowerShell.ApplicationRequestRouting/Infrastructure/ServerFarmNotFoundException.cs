6ad2c67c5639340a92b541b9333cc4b2ae3ddd37
Jon Arild T�rresdal <jon@torresdal.net>
2012-09-13 13:04:14 +0200

    Moved Arr cmdlet out from condep and into this new repo

---
+++ ConDep.PowerShell.ApplicationRequestRouting/Infrastructure/ServerFarmNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ConDep.PowerShell.ApplicationRequestRouting.Infrastructure
+{
+    public class ServerFarmNotFoundException : Exception
+    {
+        public ServerFarmNotFoundException(string message) : base(message) {}
+    }
+}
